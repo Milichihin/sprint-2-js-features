c804bd3174f189eb1dc155d0b735079e5b3bbdb6
Milichihin <32966900+Milichihin@users.noreply.github.com>
2021-06-15 00:46:20 +0300

    Question 3

--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
+<h1>SPRINT 2. Features ES 2015 - 2019</h1>
 <div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+Practical tasks. Features ES 2015 - 2019.
 </div>
 `;
 
-//============ FILTER ================
+// ================================================================
+// The function filterByN receives an array of integers,
+// a number and a parameter (greater, less).
+// Print a new array, where all elements will be greater/less than this number
+// By default, the number is 0, the parameter is greater.
 
-const filterNums = (/* arguments */) => {
-  //some code
-  return /* answer */;
+const filterNums = (arrOfNum, startingPoint = 0, comparison = "greater") => {
+  const newNums = arrOfNum.filter((num) => {
+    return comparison === "less" ? num < startingPoint : num > startingPoint;
+  });
+  return console.log(newNums);
 };
 
-filterNums([-1, 2, 4, 0, 55, -12, 3], 11, "greater"); //[ 55]
-// filterNums([-2, 2, 3, 0, 43, -13, 6], 6, 'less'); // [-2, 2, 3, 0, -13]
-// filterNums([-2, 2, 3, 0, 43, -13, 6], -33, 'less'); //  []
+// filterNums([-1, 2, 4, 0, 55, -12, 3], 11, "greater"); //[ 55]
+// filterNums([-2, 2, 3, 0, 43, -13, 6], 6, "less"); // [-2, 2, 3, 0, -13]
+// filterNums([-2, 2, 3, 0, 43, -13, 6], -33, "less"); //  []
 // filterNums([-2, 2, 3, 0, 43, -13, 6]); // [2, 3, 43, 6]
-// filterNums([-2, 2, 3, 0, 43, -13, 6], 23);  // [43]
+// filterNums([-2, 2, 3, 0, 43, -13, 6], 23); // [43]
+
+//====================================================================
+// The function takes any number of strings and returns the sum of their lengths.
+
+const sumOfLen = (...strings) => {
+  return strings.reduce((previous, current) => {
+    return previous + current;
+  }, []).length;
+};
+
+// console.log(sumOfLen("hello", "hi")); //7
+// console.log(sumOfLen("hi")); //2
+// console.log(sumOfLen()); //0
+// console.log(sumOfLen("hello", "hi", "my name", "is")); //16
+
+//====================================================================
+
+// The user enters as arguments the number of seconds, minutes, hours, days, weeks, months, years.
+// Output how many seconds are in all this.
+// All parameters are optional. Consider 30 days in a month
+
+const howMuchSec = () => {
+  //...
+};
+
+howMuchSec(12, 3); //192
+howMuchSec(1, 33, 22); //81181
+howMuchSec(); //0
