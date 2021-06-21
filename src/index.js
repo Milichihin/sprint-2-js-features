afc875b16729bde369c5aa6acbe0ff8806c78537
Milichihin <32966900+Milichihin@users.noreply.github.com>
2021-06-21 09:44:09 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
@@ -7,74 +9,74 @@ Practical tasks. Features ES 2015 - 2019.
 </div>
 `;
 
-// ================================================================
+console.log("=======================    Question 1   =======================");
 // The function filterByN receives an array of integers,
 // a number and a parameter (greater, less).
 // Print a new array, where all elements will be greater/less than this number
 // By default, the number is 0, the parameter is greater.
 
-// const filterNums = (arrOfNum, startingPoint = 0, comparison = "greater") => {
-//   const newNums = arrOfNum.filter((num) => {
-//     return comparison === "less" ? num < startingPoint : num > startingPoint;
-//   });
-//   return console.log(newNums);
-// };
+const filterNums = (arrOfNum, startingPoint = 0, comparison = "greater") => {
+  const newNums = arrOfNum.filter((num) => {
+    return comparison === "less" ? num < startingPoint : num > startingPoint;
+  });
+  return console.log(newNums);
+};
 
-// filterNums([-1, 2, 4, 0, 55, -12, 3], 11, "greater"); //[ 55]
-// filterNums([-2, 2, 3, 0, 43, -13, 6], 6, "less"); // [-2, 2, 3, 0, -13]
-// filterNums([-2, 2, 3, 0, 43, -13, 6], -33, "less"); //  []
-// filterNums([-2, 2, 3, 0, 43, -13, 6]); // [2, 3, 43, 6]
-// filterNums([-2, 2, 3, 0, 43, -13, 6], 23); // [43]
+filterNums([-1, 2, 4, 0, 55, -12, 3], 11, "greater"); //[ 55]
+filterNums([-2, 2, 3, 0, 43, -13, 6], 6, "less"); // [-2, 2, 3, 0, -13]
+filterNums([-2, 2, 3, 0, 43, -13, 6], -33, "less"); //  []
+filterNums([-2, 2, 3, 0, 43, -13, 6]); // [2, 3, 43, 6]
+filterNums([-2, 2, 3, 0, 43, -13, 6], 23); // [43]
 
-//====================================================================
+console.log("=======================    Question 2   =======================");
 // The function takes any number of strings and returns the sum of their lengths.
 
-// const sumOfLen = (...strings) => {
-// return strings.reduce((previous, current) => {
-//   return previous + current;
-// }, []).length;
-// };
+const sumOfLen = (...strings) => {
+  return strings.reduce((previous, current) => {
+    return previous + current;
+  }, []).length;
+};
 
-// console.log(sumOfLen("hello", "hi")); //7
-// console.log(sumOfLen("hi")); //2
-// console.log(sumOfLen()); //0
-// console.log(sumOfLen("hello", "hi", "my name", "is")); //16
+console.log(sumOfLen("hello", "hi")); //7
+console.log(sumOfLen("hi")); //2
+console.log(sumOfLen()); //0
+console.log(sumOfLen("hello", "hi", "my name", "is")); //16
 
-//====================================================================
+console.log("=======================    Question 3   =======================");
 
 // The user enters as arguments the number of seconds, minutes, hours, days, weeks, months, years.
 // Output how many seconds are in all this.
 // All parameters are optional. Consider 30 days in a month
 
-// const howMuchSec = (...input) => {
-//   let arrOfSeconds = [];
-//   for (let i = 0; i < input.length; i++) {
-//     i === 0
-//       ? arrOfSeconds.push(input[i])
-//       : i === 1
-//       ? arrOfSeconds.push(input[i] * 60)
-//       : i === 2
-//       ? arrOfSeconds.push(input[i] * 3600)
-//       : i === 3
-//       ? arrOfSeconds.push(input[i] * 86400)
-//       : i === 4
-//       ? arrOfSeconds.push(input[i] * 604800)
-//       : i === 5
-//       ? arrOfSeconds.push(input[i] * 18144000)
-//       : arrOfSeconds.push(input[6] * 217728000);
-//   }
+const howMuchSec = (...input) => {
+  let arrOfSeconds = [];
+  for (let i = 0; i < input.length; i++) {
+    i === 0
+      ? arrOfSeconds.push(input[i])
+      : i === 1
+      ? arrOfSeconds.push(input[i] * 60)
+      : i === 2
+      ? arrOfSeconds.push(input[i] * 3600)
+      : i === 3
+      ? arrOfSeconds.push(input[i] * 86400)
+      : i === 4
+      ? arrOfSeconds.push(input[i] * 604800)
+      : i === 5
+      ? arrOfSeconds.push(input[i] * 18144000)
+      : arrOfSeconds.push(input[6] * 217728000);
+  }
 
-//   const sumOfSeconds = arrOfSeconds.reduce((previous, current) => {
-//     return previous + current;
-//   }, 0);
-//   return console.log(sumOfSeconds);
-// };
+  const sumOfSeconds = arrOfSeconds.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
+  return console.log(sumOfSeconds);
+};
 
-// howMuchSec(12, 3); //192
-// howMuchSec(1, 33, 22); //81181
-// howMuchSec(); //0
+howMuchSec(12, 3); //192
+howMuchSec(1, 33, 22); //81181
+howMuchSec(); //0
 
-//======================================================
+console.log("=======================    Question 4   =======================");
 
 //Find the maximum interval between two consecutive numbers. Numbers are entered as arguments
 
@@ -94,4 +96,4 @@ console.log(maxInterv(3, 5, 2, 7, 11, 0, -2)); //11
 console.log(maxInterv(3, 5)); //2
 console.log(maxInterv(3)); //0
 
-// console.log(maxInterv(3));
+console.log(maxInterv(3));
